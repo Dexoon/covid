7d47c09e40dfca1ed7775ce4e370cf9ad4e39a44
Ilya <dexoon@gmail.com>
2020-04-13 17:23:32 +0300

    minor fixes to make capistrano work

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,11 +4,10 @@ lock "~> 3.13.0"
 set :application, "covid"
 set :repo_url, "git@github.com:dexoon/covid.git"
 
-
 set :deploy_to, "/home/deploy/#{fetch :application}"
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
-set :linked_files, fetch(:linked_files, []).push("config/master.key")
+
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 5
 # Default branch is :master
